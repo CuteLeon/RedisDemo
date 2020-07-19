f137bb026ad9b45fefe49ccdbec8a8012a7d6c92
Leon <Leon.Engineer@hotmail.com>
2020-07-19 23:51:09 +0800

    增加两种Redis对应的Controller和View；

---
+++ RedisDemo/Controllers/RedisCachingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace RedisDemo.Controllers
+{
+    public class RedisCachingController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDistributedCache cache;
+
+        public RedisCachingController(
+            ILogger<HomeController> logger,
+            IDistributedCache cache)
+        {
+            _logger = logger;
+            this.cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            //cache.GetAsync();
+            //cache.GetStringAsync();
+            //cache.RefreshAsync();
+            //cache.RemoveAsync();
+            //cache.SetAsync();
+            //cache.SetStringAsync();
+            return View();
+        }
+    }
+}
---
+++ RedisDemo/Controllers/RedisStackExchangeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedisDemo.Controllers
+{
+    public class RedisStackExchangeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
