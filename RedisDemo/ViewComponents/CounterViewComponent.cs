8159471c8c74561f2aadf1cd01bf9ec54c7ae0df
Leon <Leon.Engineer@hotmail.com>
2020-07-20 22:26:21 +0800

    使用视图组件实现一个计数器；

---
+++ RedisDemo/ViewComponents/CounterViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+using RedisDemo.ViewModels;
+
+namespace RedisDemo.ViewComponents
+{
+    public class CounterViewComponent : ViewComponent
+    {
+        private readonly IDatabase database;
+
+        public CounterViewComponent(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var connectId = this.HttpContext.Connection.Id;
+            var streamId = this.HttpContext.Features.Get<IHttp2StreamIdFeature>().StreamId;
+
+            await database.StringSetAsync(connectId, 0, TimeSpan.FromMinutes(10), When.NotExists);
+            await database.StringIncrementAsync(connectId);
+            var count = int.Parse(await database.StringGetAsync(connectId));
+
+            return this.View(new CounterViewModel(connectId, streamId, count));
+        }
+    }
+}
---
+++ RedisDemo/ViewModels/CounterViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RedisDemo.ViewModels
+{
+    public class CounterViewModel
+    {
+        public CounterViewModel(string connectID, int streamID, int count)
+        {
+            ConnectID = connectID;
+            StreamID = streamID;
+            Count = count;
+        }
+
+        public string ConnectID { get; set; }
+
+        public int StreamID { get; set; }
+
+        public int Count { get; set; }
+    }
+}
